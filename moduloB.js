4d9e48f8d4d3a653a6bffc4b99bdf1f56406799c
Myck William dos Santos <myckwilliam@gmail.com>
2021-05-03 19:49:49 -0300

    conceptsActualization

--- moduloB.js
+++ moduloB.js
@@ -2,5 +2,9 @@ module.exports = {
     bomDia: 'Bom dia',
     boaNoite() {
         return 'Boa noite'
+    },
+    boaTarde: {
+        bomHumor: 'Tarde top!',
+        mauHumor: 'Boa pra quem?!'
     }
 }
---
+++ moduloCliente.js
@@ -0,0 +1,7 @@
+const {ola, bemVindo, ateLogo} = require('./moduloA')
+const { boaTarde, boaNoite, bomDia } = require('./moduloB')
+const moduloB = require('./moduloB')
+console.log(ateLogo)
+console.log(bomDia);
+console.log(boaTarde.bomHumor)
+console.log(boaNoite())
